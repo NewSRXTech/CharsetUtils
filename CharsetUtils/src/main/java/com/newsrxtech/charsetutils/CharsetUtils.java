24f5be1857089cf2586bdad1448b825d3ce58be2
Michael Joyner (office computer) <michael@newsrx.com>
2017-02-27 11:29:05 -0500

    configurable logging - sort of javadoc it a little

--- CharsetUtils/src/main/java/com/newsrxtech/charsetutils/CharsetUtils.java
+++ CharsetUtils/src/main/java/com/newsrxtech/charsetutils/CharsetUtils.java
@@ -5,6 +5,8 @@ import java.nio.charset.CharsetEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.apache.commons.lang3.text.translate.AggregateTranslator;
 import org.apache.commons.lang3.text.translate.CharSequenceTranslator;
@@ -13,8 +15,23 @@ import org.apache.commons.lang3.text.translate.LookupTranslator;
 import org.apache.commons.lang3.text.translate.NumericEntityEscaper;
 
 import com.ibm.icu.text.Transliterator;
+import com.newsrxtech.charsetutils.log.Log;
 
 public class CharsetUtils {
+
+	protected static Logger log = Log.getLogger(CharsetUtils.class.getName());
+
+	static {
+		log.setLevel(Level.WARNING);
+	}
+
+	public static Level getLogLevel() {
+		return log.getLevel();
+	}
+
+	public static void setLogLevel(Level newLevel) {
+		log.setLevel(newLevel);
+	}
 
 	/**
 	 * Lowest common charset for Latin based orthographies.
@@ -29,11 +46,17 @@ public class CharsetUtils {
 	 */
 	public static final Charset ISO_8859_1 = StandardCharsets.ISO_8859_1;
 
-	private static final String ANY_LATIN = "Latin-ASCII";
-	private static final String ANY_ASCII = "Any-Latin; Latin-ASCII";
-	private static Transliterator asLatin = Transliterator.getInstance(ANY_LATIN);
-	private static Transliterator asAscii = Transliterator.getInstance(ANY_ASCII);
+	protected static final String ANY_LATIN = "Latin-ASCII";
+	protected static final String ANY_ASCII = "Any-Latin; Latin-ASCII";
+	protected static Transliterator asLatin = Transliterator.getInstance(ANY_LATIN);
+	protected static Transliterator asAscii = Transliterator.getInstance(ANY_ASCII);
 
+	/**
+	 * 
+	 * @param unicode_text
+	 * @param htmlFallback
+	 * @return String mutilated as a best fit into Windows ANSI bytes.
+	 */
 	public static byte[] asCp1252Bytes(String unicode_text, boolean htmlFallback) {
 		return asCharsetBytes(unicode_text, CP1252, htmlFallback);
 	}
@@ -44,13 +67,19 @@ public class CharsetUtils {
 	 * <strong>This encoding is not identical to ISO-8859-1!</strong>
 	 * 
 	 * @param unicode_text
-	 * @return
+	 * @return String mutilated as a best fit to Windows ANSI CP1252. 
 	 */
 	public static String asCp1252SafeString(String unicode_text, boolean htmlFallback) {
 		return asCharsetSafeString(unicode_text, CP1252, htmlFallback);
 	}
 
-	public static byte[] asIso8859_1Bytes(String unicode_text, boolean htmlFallback) {
+	/**
+	 * 
+	 * @param unicode_text
+	 * @param htmlFallback
+	 * @return String mutilated as a best fit into Western European ISO8859-1 bytes.
+	 */
+	public static byte[] asIso8859_Bytes(String unicode_text, boolean htmlFallback) {
 		return asCharsetBytes(unicode_text, ISO_8859_1, htmlFallback);
 	}
 
@@ -59,12 +88,18 @@ public class CharsetUtils {
 	 * <strong>This encoding is not identical to US Windows CP1252!</strong>
 	 * 
 	 * @param unicode_text
-	 * @return
+	 * @return String mutilated as a best fit into a Western European String.
 	 */
-	public static String asIso8859_1SafeString(String unicode_text, boolean htmlFallback) {
+	public static String asIso8859_SafeString(String unicode_text, boolean htmlFallback) {
 		return asCharsetSafeString(unicode_text, ISO_8859_1, htmlFallback);
 	}
-	
+
+	/**
+	 * 
+	 * @param unicode_text
+	 * @param htmlFallback
+	 * @return String mutilated as a best fit into 7-bit range ASCII bytes.
+	 */
 	public static byte[] asAsciiBytes(String unicode_text, boolean htmlFallback) {
 		return asCharsetBytes(unicode_text, US_ASCII, htmlFallback);
 	}
@@ -73,22 +108,30 @@ public class CharsetUtils {
 	 * Best effort to re-encode string to US ASCII.
 	 * 
 	 * @param unicode_text
-	 * @return
+	 * @return String mutilated as a best fit into a 7-bit ASCII String.
 	 */
 	public static String asAsciiSafeString(String unicode_text, boolean htmlFallback) {
 		return asCharsetSafeString(unicode_text, US_ASCII, htmlFallback);
 	}
-	
+
+	/**
+	 * 
+	 * @param unicode_text
+	 * @param charset
+	 * @param htmlFallback
+	 * @return String mutilated as a best fit into the specified charset bytes.
+	 */
 	public static byte[] asCharsetBytes(String unicode_text, Charset charset, boolean htmlFallback) {
 		return asCharsetSafeString(unicode_text, charset, htmlFallback).getBytes(charset);
 	}
-	
+
 	/**
-	 * Best effort to re-encode string to specified {@linkplain Charset} via ICU4J any-latin transliterator. <br/>
+	 * Best effort to re-encode string to specified {@linkplain Charset} via
+	 * ICU4J any-latin transliterator. <br/>
 	 * Only works well for charsets that are LATIN/US-ASCII based.
 	 * 
 	 * @param unicode_text
-	 * @return
+	 * @return String mutilated as a best fit into a charset compatible String.
 	 */
 	public static String asCharsetSafeString(String unicode_text, Charset charset, boolean htmlFallback) {
 		if (unicode_text == null) {
@@ -108,11 +151,10 @@ public class CharsetUtils {
 				if (!encoder.canEncode(asAsciiLetter) && htmlFallback) {
 					String replacement = ESCAPE_EXTENDED_FALLBACK.translate(asAsciiLetter);
 					if (!alreadyLogged.contains(asAsciiLetter)) {
-						System.err.print("ESCAPE_EXTENDED_FALLBACK: '" + asAsciiLetter + "' => ");
-						System.err.println(replacement);
+						log.info("ESCAPE_EXTENDED_FALLBACK: '" + asAsciiLetter + "' => " + replacement);
 						alreadyLogged.add(asAsciiLetter);
 					}
-					asAsciiLetter=replacement;
+					asAsciiLetter = replacement;
 				}
 				sb.append(asAsciiLetter);
 				continue;
@@ -121,13 +163,10 @@ public class CharsetUtils {
 		}
 		return sb.toString();
 	}
-	
-	private final static Set<String> alreadyLogged = new HashSet<>();
-	
-	public static final CharSequenceTranslator ESCAPE_EXTENDED_FALLBACK = 
-	        new AggregateTranslator(
-	            new LookupTranslator(EntityArrays.HTML40_EXTENDED_ESCAPE()),
-	            new NumericEntityEscaper()
-	        );
-	
+
+	protected final static Set<String> alreadyLogged = new HashSet<>();
+
+	protected static final CharSequenceTranslator ESCAPE_EXTENDED_FALLBACK = new AggregateTranslator(
+			new LookupTranslator(EntityArrays.HTML40_EXTENDED_ESCAPE()), new NumericEntityEscaper());
+
 }
